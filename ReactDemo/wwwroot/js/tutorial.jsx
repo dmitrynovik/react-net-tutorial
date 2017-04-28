b42ea73e512622a7c93f1adb94a2c7fddc14f246
dmitry.novik <dmitry.novik@vivant.com.au>
2017-04-28 13:57:09 +1000

    using props

--- ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -2,8 +2,10 @@
     render: function () {
         return (
             <div className="commentList">
-                Hello, world! I am a CommentList.
-      </div>
+                <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
+            </div>
         );
     }
 });
@@ -25,6 +27,19 @@ var CommentBox = React.createClass({
                 <h1>Comments</h1>
                 <CommentList />
                 <CommentForm />
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
             </div>
         );
     }
